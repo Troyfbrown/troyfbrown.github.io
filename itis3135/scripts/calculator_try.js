5b5e8d34306fd2a4199eb1d463f5f72e95c93476
Troyfbrown <troyfbrown@gmail.com>
2023-10-18 18:00:04 -0400

    calc try

---
+++ itis3135/scripts/calculator_try.js
@@ -0,0 +1,27 @@
+function addButton() {
+    const num1 = document.getElementById("num1");
+    const num2 = document.getElementById("num2");
+    const total = num1 + num2;
+    alert(`The sum of these two numbers is ${total}`);
+}
+
+function subtractButton() {
+    const num1 = document.getElementById("num1");
+    const num2 = document.getElementById("num2");
+    const total = num1 - num2;
+    alert(`The difference of these two numbers is ${total}`);
+}
+
+function multiplyButton() {
+    const num1 = document.getElementById("num1");
+    const num2 = document.getElementById("num2");
+    const total = num1 * num2;
+    alert(`The product of these two numbers is ${total}`);
+}
+
+function divideButton() {
+    const num1 = document.getElementById("num1");
+    const num2 = document.getElementById("num2");
+    const total = num1 / num2;
+    alert(`The quotient of these two numbers is ${total}`);
+}
