134e95cc96b70bbae2bf4578d23cb3c38bc7756b
Troyfbrown <troyfbrown@gmail.com>
2023-11-29 19:51:54 -0500

    added some JS

---
+++ itis3135/client_project/scripts/gallery.js
@@ -0,0 +1,15 @@
+function displayImage(n) {
+    if (n === 1) {
+        alert("For Aubrie's first year of indoor she played vibraphone for Resistance Indoor Percussion. Based in Oklahoma, Resitance competed in open class WGI for the shortened 2020 season.");
+    } else if (n === 2) {
+        alert("Post COVID, Aubrie returned to Resistance Indoor Percussion for the 2022 season. This time playing marimba for the ensemble.");
+    } else if (n === 3) {
+        alert("For her first season of Drum Corps, Aubrie competed played xylophone and glockenspiel for the Madison Scouts in 2022. The world class DCI ensemble placed 15th at world championships that year.");
+    } else if (n === 4) {
+        alert("Going into her 3rd year of indoor, Aubrie marched with the Ohio based group Rhythm X for their 2023. She played vibraphone this year, helping the ensemble to win a bronze medal at world championships.");
+    } else if (n === 5) {
+        alert("For her ageout year of DCI Aubrie performed with the 2023 Sacramento Mandarins. Aubrie played vibraphone for the ensemble's record breaking year in which they placed 6th for the first time ever at world championship finals.");
+    } else {
+        alert("You've clicked a non-existent image");
+    }
+}
---
+++ itis3135/client_project/scripts/service.js
@@ -0,0 +1,3 @@
+function calculateService() {
+    alert("placeholder");
+}
